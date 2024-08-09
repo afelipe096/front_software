35de9e69634496a9773935c78cc0756409bed98c
felipe currea <afelipe096@hotmail.com>
2024-08-08 19:15:53 -0500

    actualizacion true

--- src/app/services/api-general.service.ts
+++ src/app/services/api-general.service.ts
@@ -7,7 +7,7 @@ import { productos } from '../components/models/productos';
 })
 export class ApiGeneralService {
     urlApi: string = 'http://3.145.120.149:5200/api/facturacion';
-    seLogueo: any = false
+    seLogueo: any = true
 
 
     constructor(private http: HttpClient) { }
